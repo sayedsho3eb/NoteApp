64fab935eae5701ab98325ff2136f53a17224931
saydshoieb <103608170+sayedsho3eb@users.noreply.github.com>
2026-07-29 16:00:29 +0300

    create custom add button sheet

---
+++ lib/views/Widgets/cuton_button.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+class CustomButton extends StatelessWidget {
+  const CustomButton({super.key, required this.ontap});
+final VoidCallback ontap;
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: ontap,
+      child: Container(
+        height: 55,
+        width: MediaQuery.of(context).size.width,
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(8),
+          color: Colors.blueAccent,
+        ),
+        child: Center(
+          child: Text('Add', style: TextStyle(color: Colors.black, fontSize: 24)),
+        ),
+      ),
+    );
+  }
+}
--- lib/views/Widgets/show_bottom_sheet.dart
+++ lib/views/Widgets/show_bottom_sheet.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:note_app/views/Widgets/custom_text_field.dart';
+import 'package:note_app/views/Widgets/cuton_button.dart';
 
 class ShowBottomSheet extends StatelessWidget {
   const ShowBottomSheet({super.key});
@@ -8,12 +9,16 @@ class ShowBottomSheet extends StatelessWidget {
   Widget build(BuildContext context) {
     return Padding(
       padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 32),
-      child: Column(
-        children: [
-          CustomTextField(hintText: 'Title'),
-          SizedBox(height: 16),
-          CustomTextField(hintText: 'contant', maxLine: 5),
-        ],
+      child: SingleChildScrollView(
+        child: Column(
+          children: [
+            CustomTextField(hintText: 'Title'),
+            SizedBox(height: 16),
+            CustomTextField(hintText: 'contant', maxLine: 5),
+            SizedBox(height: 100),
+            CustomButton(ontap: () {}),
+          ],
+        ),
       ),
     );
   }
